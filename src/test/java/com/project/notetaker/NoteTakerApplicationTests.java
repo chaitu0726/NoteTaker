0c34dc01603a3afdc69dbd910b4dcf4689ff2362
chaitu0726 <cmh24292@gmail.com>
2021-08-19 12:30:46 +0530

    Intitial

---
+++ src/test/java/com/project/notetaker/NoteTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.notetaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
